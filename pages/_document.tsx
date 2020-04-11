72973448a18e26a462d7e6b4475107a7b3fd5b36
Ondřej Bárta <email@ondrejbarta.xyz>
2020-04-11 23:21:14 +0200

    Add styled-components ssr

---
+++ pages/_document.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import NextDocument from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
+
+export default class Document extends NextDocument {
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps = await NextDocument.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
