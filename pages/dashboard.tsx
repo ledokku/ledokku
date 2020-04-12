126f490d6db531abf2745e6e0ff6546a8a535b87
pradel <pradel.leo@gmail.com>
2020-04-12 17:56:31 +0200

    dashboard links

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -119,7 +119,13 @@ const Dashboard = () => {
                         />
                       ))}
 
-                      <BoxButton label="New App" icon={<Plus />} />
+                      <BoxButton
+                        label="New App"
+                        icon={<Plus />}
+                        onClick={() =>
+                          router.push(`/server/${server.id}/create-app`)
+                        }
+                      />
                     </Grid>
                   </Box>
 
@@ -135,7 +141,13 @@ const Dashboard = () => {
                           icon={<PostgreSQLIcon size={24} />}
                         />
                       ))}
-                      <BoxButton label="New Database" icon={<Plus />} />
+                      <BoxButton
+                        label="New Database"
+                        icon={<Plus />}
+                        onClick={() =>
+                          router.push(`/server/${server.id}/create-database`)
+                        }
+                      />
                     </Grid>
                   </Box>
                 </Grid>
