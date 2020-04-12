4c469c183b97b56c8498921314a4d5d0a051803f
pradel <pradel.leo@gmail.com>
2020-04-12 12:54:25 +0200

    style create server

--- pages/onboarding/create-server.tsx
+++ pages/onboarding/create-server.tsx
@@ -1,13 +1,23 @@
 import React from 'react';
 import { useRouter } from 'next/router';
 import { useFormik } from 'formik';
+import { ArrowRight } from 'react-feather';
 
 import { useCreateDigitalOceanServerMutation } from '../../src/generated/graphql';
 import { OnboardingLayout } from '../../layouts/OnboardingLayout';
-import { Button } from '../../ui/components/Button';
 import { Headline } from '../../ui/components/Typography/components/Headline';
 import { Paragraph } from '../../ui/components/Typography/components/Paragraph';
 import withApollo from '../../lib/withApollo';
+import {
+  TextField,
+  Button,
+  styled,
+  Flex,
+  Typography,
+  Box,
+  BoxButton,
+  Grid,
+} from '../../ui';
 
 const CreateServer = () => {
   const router = useRouter();
@@ -43,18 +53,31 @@ const CreateServer = () => {
         },
       ]}
     >
-      <form onSubmit={formik.handleSubmit}>
-        <p>Enter a name for your new server</p>
-        <input
+      <Form onSubmit={formik.handleSubmit}>
+        <TextField
           id="name"
           name="name"
+          label="Enter a name for your new server"
           value={formik.values.name}
           onChange={formik.handleChange}
         />
-        <button type="submit">Submit</button>
-      </form>
+        <Flex justifyContent="flex-end">
+          <Button type="submit" background="primary" endIcon={<ArrowRight />}>
+            Submit
+          </Button>
+        </Flex>
+      </Form>
     </OnboardingLayout>
   );
 };
+
+const Form = styled.form`
+  display: grid;
+  grid-row-gap: 80px;
+
+  @media ${({ theme }) => theme.media.phone} {
+    grid-row-gap: 40px;
+  }
+`;
 
 export default withApollo(CreateServer);
