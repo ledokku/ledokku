428f6a8c7b5d2d2d8c5f9ea2abbffd68975e4a36
pradel <pradel.leo@gmail.com>
2020-04-30 18:23:49 +0200

    handle expired tokens

--- client/src/lib/withApollo.tsx
+++ client/src/lib/withApollo.tsx
@@ -1,8 +1,11 @@
 import withApollo from 'next-with-apollo';
+import { Router } from 'next/router';
 import { ApolloClient } from 'apollo-client';
+import { from } from 'apollo-link';
 import { createHttpLink } from 'apollo-link-http';
 import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { config } from '../config';
 
@@ -22,8 +25,22 @@ export default withApollo(
       };
     });
 
+    const errorLink = onError(({ graphQLErrors }) => {
+      if (graphQLErrors) {
+        const error = graphQLErrors[0];
+        /**
+         * If we get the "Unauthorized" message from the server
+         * it means the token is not valid anymore so we log out the user
+         */
+        if (error.message === 'Unauthorized') {
+          localStorage.removeItem('accessToken');
+          window.location.href = '/';
+        }
+      }
+    });
+
     const client = new ApolloClient({
-      link: authLink.concat(httpLink),
+      link: from([authLink, errorLink, httpLink]),
       cache: new InMemoryCache().restore(initialState || {}),
     });
 
