4ce59f4a74d032bdd25fb4d03facaf459a7fdb0d
arturs <arturs@Arturss-MBP.fritz.box>
2020-05-03 17:15:38 +0200

    Add server side validation fo saving DO access token and server creation

--- server/src/graphql/mutations/saveDigitalOceanAccessToken.ts
+++ server/src/graphql/mutations/saveDigitalOceanAccessToken.ts
@@ -1,6 +1,16 @@
 import fetch from 'node-fetch';
+import * as yup from 'yup';
+import { digitalOceanAccessTokenRegExp } from './../utils';
 import { MutationResolvers } from '../../generated/graphql';
 import { prisma } from '../../prisma';
+
+// TODO unit test this schema
+const saveDigitalOceanTokenSchema = yup.object({
+  digitalOceanAccessToken: yup
+    .string()
+    .required()
+    .matches(digitalOceanAccessTokenRegExp),
+});
 
 export const saveDigitalOceanAccessToken: MutationResolvers['saveDigitalOceanAccessToken'] = async (
   _,
@@ -10,6 +20,9 @@ export const saveDigitalOceanAccessToken: MutationResolvers['saveDigitalOceanAcc
   if (!userId) {
     throw new Error('Unauthorized');
   }
+
+  // We make sure the token is valid to avoid security risks
+  saveDigitalOceanTokenSchema.validateSync({ digitalOceanAccessToken });
 
   try {
     const resData = await fetch('https://api.digitalocean.com/v2/account', {
---
+++ server/src/graphql/utils.ts
@@ -0,0 +1,2 @@
+// Digital ocean token format = exactly 64 chars, lowercase letters & numbers
+export const digitalOceanAccessTokenRegExp = /^[a-z0-9]{64}/;
