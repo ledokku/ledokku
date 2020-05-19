b80ec30abec374724f3c01eecf58846fe6d56b55
pradel <pradel.leo@gmail.com>
2020-05-19 08:14:52 +0200

    link databases to the app in docker

--- server/src/queues/buildApp.ts
+++ server/src/queues/buildApp.ts
@@ -63,7 +63,7 @@ const worker = new Worker(
 
     // We send multiple logs at once to not spam the client
     let logs: RealtimeLog[] = [];
-    let logTimerId: number;
+    let logTimerId: NodeJS.Timeout;
     const sendLogs = (log: RealtimeLog) => {
       logs.push(log);
       clearTimeout(logTimerId);
