9b141984f0e7a573c575b7b82c0e987f99378b17
arturs <arturs@Arturss-MBP.fritz.box>
2020-06-05 09:20:17 +0200

    Add dynamic button name in env var section

--- client/src/pages/app/[appId]/env.tsx
+++ client/src/pages/app/[appId]/env.tsx
@@ -97,7 +97,7 @@ const EnvForm = ({ name, value, appId, isNewVar }: EnvFormProps) => {
             <path d="M.2 10a11 11 0 0 1 19.6 0A11 11 0 0 1 .2 10zm9.8 4a4 4 0 1 0 0-8 4 4 0 0 0 0 8zm0-2a2 2 0 1 1 0-4 2 2 0 0 1 0 4z" />
           </svg>
           <button className="inline py-2 px-10 bg-gray-900 hover:bg-blue text-white  font-bold hover:text-white border hover:border-transparent rounded-lg">
-            Save
+            {isNewVar ? 'Add' : 'Save'}
           </button>
         </div>
       </div>
