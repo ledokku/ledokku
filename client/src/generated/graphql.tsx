36275c514cc7d5443effa850cab1ba4adcfa36c1
arturs <arturs@Arturss-MBP.fritz.box>
2020-06-06 18:39:42 +0200

    Add generated file

--- client/src/generated/graphql.tsx
+++ client/src/generated/graphql.tsx
@@ -12,7 +12,7 @@ export type Scalars = {
   Int: number;
   Float: number;
   /** Use JavaScript Date object for date/time fields. */
-  DateTime: String;
+  DateTime: string;
   /** The `Upload` scalar type represents a file upload. */
   Upload: any;
 };
