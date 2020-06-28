1399b8f82c7121e372ed304e0cae6cb214aa8110
arturs <arturs@Arturss-MBP.fritz.box>
2020-06-28 16:25:53 +0200

    Upd button component

--- client/src/ui/components/Button.tsx
+++ client/src/ui/components/Button.tsx
@@ -13,38 +13,36 @@ interface ButtonProps {
   onClick?(event?: React.FormEvent): void;
 }
 
-export const Button = ({ children, ...props }: ButtonProps) => {
-  const {
-    iconEnd,
-    iconStart,
-    color,
-    disabled,
-    className,
-    size,
-    isSubmit,
-    onClick,
-  } = props;
-  return (
-    <button
-      onClick={onClick}
-      type={isSubmit ? 'submit' : 'button'}
-      className={cx(
-        'py-2 px-10 bg-gray-900 hover:bg-blue text-white  font-bold  rounded-lg flex justify-center',
-        {
-          'bg-gray-900': color === 'grey',
-          'bg-red-500': color === 'red',
-          'opacity-50 cursor-not-allowed': disabled,
-          'hover:text-white border hover:border-transparent': !disabled,
-          'w-64': size === 'large',
-          'w-32': size === 'normal',
-        },
-        className
-      )}
-    >
-      {iconStart && <span className="-ml-4 mr-4 -px-4">{iconStart}</span>}
-      {children}
-
-      {iconEnd && <span className="ml-6 -mr-4 -px-4">{iconEnd}</span>}
-    </button>
-  );
-};
+export const Button = ({
+  children,
+  isSubmit,
+  color,
+  disabled,
+  size,
+  className,
+  iconEnd,
+  iconStart,
+  ...props
+}: ButtonProps) => (
+  <button
+    {...props}
+    disabled={disabled}
+    type={isSubmit ? 'submit' : 'button'}
+    className={cx(
+      'py-2 px-10 bg-gray-900 hover:bg-blue text-white  font-bold  rounded-lg flex justify-center',
+      {
+        'bg-gray-900': color === 'grey',
+        'bg-red-500': color === 'red',
+        'opacity-50 cursor-not-allowed': disabled,
+        'hover:text-white border hover:border-transparent': !disabled,
+        'w-64': size === 'large',
+        'w-32': size === 'normal',
+      },
+      className
+    )}
+  >
+    {iconStart && <span className="-ml-4 mr-4 -px-4">{iconStart}</span>}
+    {children}
+    {iconEnd && <span className="ml-6 -mr-4 -px-4">{iconEnd}</span>}
+  </button>
+);
