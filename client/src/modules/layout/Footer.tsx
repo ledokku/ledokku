3a856f2feb16517f05ee9a6e2e082d9d94223f93
pradel <pradel.leo@gmail.com>
2020-07-19 14:03:13 +0200

    create footer

---
+++ client/src/modules/layout/Footer.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Twitter, GitHub } from 'react-feather';
+
+export const Footer = () => {
+  return (
+    <footer className="bg-gray-200">
+      <div className="max-w-5xl mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
+        <p className="text-gray-500 text-sm text-center sm:text-left">
+          © {new Date().getFullYear()} ledokku
+        </p>
+        <span className="inline-flex sm:ml-auto sm:mt-0 mt-2 justify-center sm:justify-start">
+          <a
+            className="text-gray-500"
+            href="https://twitter.com/ledokku"
+            target="_blank"
+          >
+            <Twitter className="w-5 h-5" />
+          </a>
+          <a
+            className="ml-3 text-gray-500"
+            href="https://github.com/ledokku/ledokku"
+            target="_blank"
+          >
+            <GitHub className="w-5 h-5" />
+          </a>
+        </span>
+      </div>
+    </footer>
+  );
+};
--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { useDashboardQuery } from '../generated/graphql';
 import { NodeIcon } from '../ui/icons/NodeIcon';
 import { Header } from '../modules/layout/Header';
+import { Footer } from '../modules/layout/Footer';
 import { TabNav, TabNavLink, Button } from '../ui';
 import { PostgreSQLIcon } from '../ui/icons/PostgreSQLIcon';
 
@@ -18,65 +19,68 @@ export const Dashboard = () => {
   // };
 
   return (
-    <div>
-      <Header />
+    <div className="h-screen flex justify-between flex-col">
+      <div>
+        <Header />
+        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+          <TabNav>
+            <TabNavLink to="/dashboard" selected>
+              Dashboard
+            </TabNavLink>
+            <TabNavLink to="/activity">Activity</TabNavLink>
+            <TabNavLink to="/metrics">Metrics</TabNavLink>
+            <TabNavLink to="/settings">Settings</TabNavLink>
+          </TabNav>
+        </div>
 
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-        <TabNav>
-          <TabNavLink to="/dashboard" selected>
-            Dashboard
-          </TabNavLink>
-          <TabNavLink to="/activity">Activity</TabNavLink>
-          <TabNavLink to="/metrics">Metrics</TabNavLink>
-          <TabNavLink to="/settings">Settings</TabNavLink>
-        </TabNav>
+        <div className="max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+          <main className="grid grid-cols-12 col-gap-20">
+            <div className="col-span-7 mt-4">
+              <h1 className="text-lg font-bold py-5 ">Apps</h1>
+              {data?.apps.map((app) => (
+                <div key={app.id}>
+                  <Link to={`/app/${app.id}`} className="py-2 block">
+                    <div className="flex items-center py-3 px-2 shadow hover:shadow-md transition-shadow duration-100 ease-in-out rounded bg-white">
+                      <NodeIcon size={20} className="mr-2" /> {app.name}
+                    </div>
+                  </Link>
+                </div>
+              ))}
+              <p className="py-3">
+                <Link to="/create-app">
+                  <Button width="large" color={'grey'}>
+                    Create new app
+                  </Button>
+                </Link>
+              </p>
+              <h1 className="text-lg font-bold py-5">Databases</h1>
+              {data?.databases.map((database) => (
+                <div key={database.id}>
+                  <Link to={`/database/${database.id}`} className="py-2 block">
+                    <div className="flex items-center py-3 px-2 shadow hover:shadow-md transition-shadow duration-100 ease-in-out rounded bg-white">
+                      <PostgreSQLIcon size={24} className="mr-2" />
+                      <p>{database.name}</p>
+                    </div>
+                  </Link>
+                </div>
+              ))}
+              <p className="py-3">
+                <Link to="/create-database">
+                  <Button width="large" color={'grey'}>
+                    Create new database
+                  </Button>
+                </Link>
+              </p>
+            </div>
+            <div className="col-span-5 mt-4">
+              <h1 className="text-lg font-bold py-5">Latest activity</h1>
+              <p className="text-cool-gray-400">Coming soon</p>
+            </div>
+          </main>
+        </div>
       </div>
 
-      <div className="max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-        <main className="grid grid-cols-12 col-gap-20">
-          <div className="col-span-7 mt-4">
-            <h1 className="text-lg font-bold py-5 ">Apps</h1>
-            {data?.apps.map((app) => (
-              <div key={app.id}>
-                <Link to={`/app/${app.id}`} className="py-2 block">
-                  <div className="flex items-center py-3 px-2 shadow hover:shadow-md transition-shadow duration-100 ease-in-out rounded bg-white">
-                    <NodeIcon size={20} className="mr-2" /> {app.name}
-                  </div>
-                </Link>
-              </div>
-            ))}
-            <p className="py-3">
-              <Link to="/create-app">
-                <Button width="large" color={'grey'}>
-                  Create new app
-                </Button>
-              </Link>
-            </p>
-            <h1 className="text-lg font-bold py-5">Databases</h1>
-            {data?.databases.map((database) => (
-              <div key={database.id}>
-                <Link to={`/database/${database.id}`} className="py-2 block">
-                  <div className="flex items-center py-3 px-2 shadow hover:shadow-md transition-shadow duration-100 ease-in-out rounded bg-white">
-                    <PostgreSQLIcon size={24} className="mr-2" />
-                    <p>{database.name}</p>
-                  </div>
-                </Link>
-              </div>
-            ))}
-            <p className="py-3">
-              <Link to="/create-database">
-                <Button width="large" color={'grey'}>
-                  Create new database
-                </Button>
-              </Link>
-            </p>
-          </div>
-          <div className="col-span-5 mt-4">
-            <h1 className="text-lg font-bold py-5">Latest activity</h1>
-            <p className="text-cool-gray-400">Coming soon</p>
-          </div>
-        </main>
-      </div>
+      <Footer />
     </div>
   );
 };
