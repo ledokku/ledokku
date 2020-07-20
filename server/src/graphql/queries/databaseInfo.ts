48db5cc3c3cd2a27b76942dc19ce4237f1fa5772
arturs <arturs.kirtovskis@gmail.com>
2020-07-20 23:26:31 +0200

    Add back databas info file

---
+++ server/src/graphql/queries/databaseInfo.ts
@@ -0,0 +1,33 @@
+import { dbTypeToDokkuPlugin } from '../utils';
+import { QueryResolvers } from '../../generated/graphql';
+import { sshConnect } from '../../lib/ssh';
+import { dokku } from '../../lib/dokku';
+import { prisma } from '../../prisma';
+
+export const databaseInfo: QueryResolvers['databaseInfo'] = async (
+  _,
+  { databaseId },
+  { userId }
+) => {
+  if (!userId) {
+    throw new Error('Unauthorized');
+  }
+
+  const database = await prisma.database.findOne({
+    where: {
+      id: databaseId,
+    },
+  });
+
+  if (!database) {
+    throw new Error(`Database with ID ${databaseId} not found`);
+  }
+
+  const dbType = dbTypeToDokkuPlugin(database.type);
+
+  const ssh = await sshConnect();
+
+  const info = await dokku.database.info(ssh, database.name, dbType);
+
+  return { info };
+};
