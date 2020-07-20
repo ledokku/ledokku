768796b6767c601a2215f28fed6e38bdfd323870
Arturs Kirtovskis <arturs.kirtovskis@gmail.com>
2020-07-20 18:39:16 +0200

    feat(client): restructure app and db management views (#101)

--- client/src/pages/database/logs.tsx
+++ client/src/pages/database/logs.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { Header } from '../../modules/layout/Header';
 import { useDatabaseByIdQuery } from '../../generated/graphql';
-import { TabNav, TabNavLink } from '../../ui';
+import { TabNav, TabNavLink, Terminal } from '../../ui';
 
-export const Apps = () => {
+export const Logs = () => {
   const { id: databaseId } = useParams();
 
   const { data, loading /* error */ } = useDatabaseByIdQuery({
@@ -39,8 +39,8 @@ export const Apps = () => {
       <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
         <TabNav>
           <TabNavLink to={`/database/${database.id}`}>Database</TabNavLink>
-          <TabNavLink to={`/database/${database.id}/apps`} selected>
-            Apps
+          <TabNavLink to={`/database/${database.id}/logs`} selected>
+            Logs
           </TabNavLink>
           <TabNavLink to={`/database/${database.id}/settings`}>
             Settings
@@ -48,19 +48,16 @@ export const Apps = () => {
         </TabNav>
       </div>
 
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="grid sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-1 gap-4 mt-10">
-          <h1 className="text-lg font-bold py-5">Apps</h1>
+          <h1 className="text-lg font-bold py-5">Logs</h1>
         </div>
-        <div className="mt-4 mb-4">
-          <h2 className="text-gray-400">
-            {`Here you can modify apps linked to:`}
-            <span className="text-gray-900"> {database.name}</span> database
-          </h2>
-        </div>
-        <button className="mt-4 bg-gray-900 hover:bg-blue text-white  font-bold hover:text-white py-2 px-4 border hover:border-transparent rounded-lg">
-          Connect app
-        </button>
+        <Terminal className="pt-8 pb-16">
+          <div className="flex">
+            <p className="flex-1 typing items-center pl-2">{`Database status:`}</p>
+            Clean
+          </div>
+        </Terminal>
       </div>
     </div>
   );
