81d132a7fe869a17f18bd6874f8c0e51254d71eb
arturs <arturs.kirtovskis@gmail.com>
2020-08-05 14:31:41 +0300

    Simplify is databaseLinked query

--- server/src/graphql/queries/isDatabaseLinked.ts
+++ server/src/graphql/queries/isDatabaseLinked.ts
@@ -9,51 +9,21 @@ export const isDatabaseLinked: QueryResolvers['isDatabaseLinked'] = async (
   if (!userId) {
     throw new Error('Unauthorized');
   }
-
-  const promiseResponse = await Promise.all([
-    prisma.database.findOne({
-      where: {
-        id: databaseId,
+  // We query for link between particular db and particular app
+  const linkedApps = await prisma.database.findOne({
+    where: {
+      id: databaseId,
+    },
+    select: {
+      apps: {
+        where: {
+          id: appId,
+        },
       },
-    }),
-    prisma.app.findOne({
-      where: {
-        id: appId,
-      },
-    }),
-    prisma.app.findOne({
-      where: {
-        id: appId,
-      },
-      select: {
-        databases: true,
-      },
-    }),
-  ]).catch((e) => {
-    throw new Error(`failed to get data from db due to:${e}`);
+    },
   });
 
-  const database = promiseResponse[0];
-  const app = promiseResponse[1];
-  const linkedDbs = promiseResponse[2];
-
-  if (!app) {
-    throw new Error(`App with ID ${appId} not found`);
-  }
-
-  if (!database) {
-    throw new Error(`Database with ID ${databaseId} not found`);
-  }
-
-  if (app.userId !== userId || database.userId !== userId) {
-    throw new Error(
-      `App with ID ${appId} or databsde with ${databaseId} does not belong to ${userId}`
-    );
-  }
-
-  const dbLinks = linkedDbs.databases.find((db) => db.id === databaseId);
-
-  const isLinked = dbLinks ? true : false;
+  const isLinked = linkedApps.apps.length === 1;
 
   return { isLinked };
 };
