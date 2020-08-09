2ca203e0ce307d1b3fa7a8b2c6c4c4629ccc1d81
pradel <pradel.leo@gmail.com>
2020-08-09 15:10:42 +0200

    remvoe footer

--- client/src/pages/dashboard.tsx
+++ client/src/pages/dashboard.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDashboardQuery } from '../generated/graphql';
 import { Header } from '../modules/layout/Header';
-import { Footer } from '../modules/layout/Footer';
 import { TabNav, TabNavLink, Button } from '../ui';
 import { PostgreSQLIcon } from '../ui/icons/PostgreSQLIcon';
 import { MongoIcon } from '../ui/icons/MongoIcon';
@@ -19,100 +18,93 @@ export const Dashboard = () => {
   // TODO if no apps or dbs show onboarding screen
 
   return (
-    <div className="h-screen flex justify-between flex-col">
-      <div>
-        <Header />
-        <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-          <TabNav>
-            <TabNavLink to="/dashboard" selected>
-              Dashboard
-            </TabNavLink>
-            <TabNavLink to="/activity">Activity</TabNavLink>
-            <TabNavLink to="/metrics">Metrics</TabNavLink>
-            <TabNavLink to="/settings">Settings</TabNavLink>
-          </TabNav>
-        </div>
-
-        <div className="max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-end pb-6">
-            <Link to="/create-database">
-              <Button color={'grey'} variant="outline" className="text-sm mr-3">
-                Create database
-              </Button>
-            </Link>
-            <Link to="/create-app">
-              <Button color={'grey'} className="text-sm">
-                Create app
-              </Button>
-            </Link>
-          </div>
-          <main className="grid grid-cols-1 md:grid-cols-12 md:col-gap-20">
-            <div className="col-span-7 mt-4">
-              <h1 className="text-lg font-bold py-2">Apps</h1>
-              {data?.apps.map((app) => (
-                <div key={app.id} className="py-3 border-b border-gray-200">
-                  <div className="mb-1 text-gray-900 font-medium">
-                    <Link to={`/app/${app.id}`}>
-                      <div>{app.name}</div>
-                    </Link>
-                  </div>
-                  <div className="flex justify-between text-gray-400 text-sm">
-                    <div>ledokku/ledokku</div>
-                    <div>Last deploy 6h ago</div>
-                  </div>
-                </div>
-              ))}
-
-              <h1 className="text-lg font-bold pb-2 pt-5">Databases</h1>
-              {data?.databases.map((database) => (
-                <div
-                  key={database.id}
-                  className="py-3 border-b border-gray-200"
-                >
-                  <div className="mb-1 text-gray-900 font-medium">
-                    <Link to={`/database/${database.id}`}>{database.name}</Link>
-                  </div>
-                  <div className="flex justify-between text-gray-400 text-sm">
-                    <div className="flex items-center">
-                      {database.type === 'POSTGRESQL' ? (
-                        <React.Fragment>
-                          <PostgreSQLIcon size={16} className="mr-1" />
-                          PostgreSQL
-                        </React.Fragment>
-                      ) : undefined}
-                      {database.type === 'MONGODB' ? (
-                        <React.Fragment>
-                          <MongoIcon size={16} className="mr-1" />
-                          Mongo
-                        </React.Fragment>
-                      ) : undefined}
-                      {database.type === 'REDIS' ? (
-                        <React.Fragment>
-                          <RedisIcon size={16} className="mr-1" />
-                          Redis
-                        </React.Fragment>
-                      ) : undefined}
-                      {database.type === 'MYSQL' ? (
-                        <React.Fragment>
-                          <MySQLIcon size={16} className="mr-1" />
-                          MySQL
-                        </React.Fragment>
-                      ) : undefined}
-                    </div>
-                    <div>Created 1h ago</div>
-                  </div>
-                </div>
-              ))}
-            </div>
-            <div className="col-span-5 mt-4">
-              <h1 className="text-lg font-bold py-5">Latest activity</h1>
-              <p className="text-gray-400 text-sm">Coming soon</p>
-            </div>
-          </main>
-        </div>
+    <div>
+      <Header />
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <TabNav>
+          <TabNavLink to="/dashboard" selected>
+            Dashboard
+          </TabNavLink>
+          <TabNavLink to="/activity">Activity</TabNavLink>
+          <TabNavLink to="/metrics">Metrics</TabNavLink>
+          <TabNavLink to="/settings">Settings</TabNavLink>
+        </TabNav>
       </div>
 
-      <Footer />
+      <div className="max-w-5xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+        <div className="flex justify-end pb-6">
+          <Link to="/create-database">
+            <Button color={'grey'} variant="outline" className="text-sm mr-3">
+              Create database
+            </Button>
+          </Link>
+          <Link to="/create-app">
+            <Button color={'grey'} className="text-sm">
+              Create app
+            </Button>
+          </Link>
+        </div>
+        <main className="grid grid-cols-1 md:grid-cols-12 md:col-gap-20">
+          <div className="col-span-7 mt-4">
+            <h1 className="text-lg font-bold py-2">Apps</h1>
+            {data?.apps.map((app) => (
+              <div key={app.id} className="py-3 border-b border-gray-200">
+                <div className="mb-1 text-gray-900 font-medium">
+                  <Link to={`/app/${app.id}`}>
+                    <div>{app.name}</div>
+                  </Link>
+                </div>
+                <div className="flex justify-between text-gray-400 text-sm">
+                  <div>ledokku/ledokku</div>
+                  <div>Last deploy 6h ago</div>
+                </div>
+              </div>
+            ))}
+
+            <h1 className="text-lg font-bold pb-2 pt-5">Databases</h1>
+            {data?.databases.map((database) => (
+              <div key={database.id} className="py-3 border-b border-gray-200">
+                <div className="mb-1 text-gray-900 font-medium">
+                  <Link to={`/database/${database.id}`}>{database.name}</Link>
+                </div>
+                <div className="flex justify-between text-gray-400 text-sm">
+                  <div className="flex items-center">
+                    {database.type === 'POSTGRESQL' ? (
+                      <React.Fragment>
+                        <PostgreSQLIcon size={16} className="mr-1" />
+                        PostgreSQL
+                      </React.Fragment>
+                    ) : undefined}
+                    {database.type === 'MONGODB' ? (
+                      <React.Fragment>
+                        <MongoIcon size={16} className="mr-1" />
+                        Mongo
+                      </React.Fragment>
+                    ) : undefined}
+                    {database.type === 'REDIS' ? (
+                      <React.Fragment>
+                        <RedisIcon size={16} className="mr-1" />
+                        Redis
+                      </React.Fragment>
+                    ) : undefined}
+                    {database.type === 'MYSQL' ? (
+                      <React.Fragment>
+                        <MySQLIcon size={16} className="mr-1" />
+                        MySQL
+                      </React.Fragment>
+                    ) : undefined}
+                  </div>
+                  <div>Created 1h ago</div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="col-span-5 mt-4">
+            <h1 className="text-lg font-bold py-5">Latest activity</h1>
+            <p className="text-gray-400 text-sm">Coming soon</p>
+          </div>
+        </main>
+      </div>
     </div>
   );
 };
