93ae58b5c13a3dbef4ca84daf8787677727eaba2
arturs <arturs.kirtovskis@gmail.com>
2020-09-28 15:42:23 +0300

    Remove socket io

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,7 +1,5 @@
 import express from 'express';
 import { createServer } from 'http';
-import socketIo from 'socket.io';
 
 export const app = express();
 export const http = createServer(app);
-export const io = socketIo(http);
