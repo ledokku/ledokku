38b90b0d4e052b144ce2c3f660a81f5f53e27e66
pradel <pradel.leo@gmail.com>
2020-10-04 15:12:36 +0200

    fix: WS production env for apollo client

--- client/src/config.ts
+++ client/src/config.ts
@@ -4,10 +4,12 @@ export const config = runConfig
   ? {
       githubClientId: runConfig['GITHUB_CLIENT_ID'],
       serverUrl: '',
+      serverWsUrl: `ws://${window.location.host}`,
       environment: process.env.NODE_ENV,
     }
   : {
       githubClientId: process.env.REACT_APP_GITHUB_CLIENT_ID,
       serverUrl: process.env.REACT_APP_SERVER_URL,
+      serverWsUrl: process.env.REACT_APP_SERVER_URL?.replace('http', 'ws'),
       environment: process.env.NODE_ENV,
     };
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -33,7 +33,7 @@ const httpLink = new HttpLink({
 });
 
 const wsLink = new WebSocketLink({
-  uri: `ws://localhost:4000/graphql`,
+  uri: `${config.serverWsUrl}/graphql`,
   options: {
     reconnect: true,
   },
