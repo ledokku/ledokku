f44a38660a5dd68c621ec9fbbd5738aac75714ad
arturs <arturs.kirtovskis@gmail.com>
2020-10-18 18:50:46 +0300

    Remove redundant import

--- client/src/pages/create-database.tsx
+++ client/src/pages/create-database.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useFormik } from 'formik';
 import * as yup from 'yup';
