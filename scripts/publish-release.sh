449e504a8920dd6cfdb60647f330175cfe1d8242
pradel <pradel.leo@gmail.com>
2020-10-23 17:39:09 +0200

    Update publish-release.sh

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -3,7 +3,7 @@ git checkout master
 git pull
 git fetch --all --tags
 
-# Get the latest tag
-LATEST_RELEASE=$(git describe --tags `git rev-list --tags --max-count=1`)
+# Get the latest version
+LATEST_RELEASE=$(jq -r ".version" server/package.json)
 
 gh release create ${LATEST_RELEASE} --title "${LATEST_RELEASE}" --notes "Take a look at the [changelog](https://www.ledokku.com/docs/changelog) to see what's new in this version ✨."
