dfbda1b69cb91810a8bca60caa902d05485dd0f9
Léo Pradel <pradel.leo@gmail.com>
2020-10-23 18:28:13 +0200

    docs: create configuration guide (#211)

--- website/sidebars.js
+++ website/sidebars.js
@@ -8,6 +8,6 @@ module.exports = {
       'installation',
       'upgrade'
     ],
-    Advanced: ['advanced/manual-installation'],
+    Advanced: ['advanced/manual-installation', 'advanced/configuration'],
   },
 };
