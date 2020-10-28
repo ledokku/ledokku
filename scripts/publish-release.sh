a50ef1af1e6e21dadecc9bf48da3423594f42501
Léo Pradel <pradel.leo@gmail.com>
2020-10-28 13:27:22 +0100

    fix: fix publish script missing release prefix

--- scripts/publish-release.sh
+++ scripts/publish-release.sh
@@ -6,4 +6,4 @@ git fetch --all --tags
 # Get the latest version
 LATEST_RELEASE=$(jq -r ".version" server/package.json)
 
-gh release create ${LATEST_RELEASE} --title "${LATEST_RELEASE}" --notes "Take a look at the [changelog](https://www.ledokku.com/docs/changelog) to see what's new in this version ✨."
+gh release create "v${LATEST_RELEASE}" --title "v${LATEST_RELEASE}" --notes "Take a look at the [changelog](https://www.ledokku.com/docs/changelog) to see what's new in this version ✨."
