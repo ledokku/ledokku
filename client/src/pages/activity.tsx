a35520f472d6d1884674e749093ef5f712c1955b
pradel <pradel.leo@gmail.com>
2021-02-01 15:49:38 +0100

    rest of root pages

--- client/src/pages/activity.tsx
+++ client/src/pages/activity.tsx
@@ -1,19 +1,13 @@
-// import { useDashboardQuery } from '../generated/graphql';
+import { Container, Heading, Text } from '@chakra-ui/react';
 import { Header } from '../modules/layout/Header';
 import { TabNav, TabNavLink } from '../ui';
 
 export const Activity = () => {
-  // const router = useRouter();
-  // const { data, loading, error } = useDashboardQuery({});
-
-  // TODO show loading
-  // TODO handle error
-
   return (
     <div>
       <Header />
 
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+      <Container maxW="5xl">
         <TabNav>
           <TabNavLink to="/dashboard">Dashboard</TabNavLink>
           <TabNavLink to="/activity" selected>
@@ -22,14 +16,16 @@ export const Activity = () => {
           <TabNavLink to="/metrics">Metrics</TabNavLink>
           <TabNavLink to="/settings">Settings</TabNavLink>
         </TabNav>
-      </div>
+      </Container>
 
-      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 mt-10">
-          <h1 className="text-lg font-bold py-5">Activity</h1>
-        </div>
-        <p className="text-gray-400 text-sm">Coming soon</p>
-      </div>
+      <Container maxW="5xl" mt={10}>
+        <Heading as="h2" size="md" py={5}>
+          Activity
+        </Heading>
+        <Text fontSize="sm" color="gray.400">
+          Coming soon
+        </Text>
+      </Container>
     </div>
   );
 };
