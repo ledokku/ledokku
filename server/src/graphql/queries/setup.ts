26667cc229e448970483d095d783c8fc94c1b9e6
Léo Pradel <pradel.leo@gmail.com>
2021-02-26 12:16:45 +0100

    feat: add debug logs for the setup query (#274)

--- server/src/graphql/queries/setup.ts
+++ server/src/graphql/queries/setup.ts
@@ -1,7 +1,10 @@
 import { readFileSync } from 'fs';
+import createDebug from 'debug';
 import { QueryResolvers } from '../../generated/graphql';
 import { config } from '../../config';
 import { sshConnect } from '../../lib/ssh';
+
+const debug = createDebug(`queries:setup`);
 
 export const setup: QueryResolvers['setup'] = async () => {
   const publicKey = readFileSync(`${config.sshKeyPath}.pub`, {
@@ -14,6 +17,7 @@ export const setup: QueryResolvers['setup'] = async () => {
     canConnectSsh = true;
   } catch (error) {
     // We do nothing as canConnectSsh is false
+    debug(error);
   }
 
   return {
