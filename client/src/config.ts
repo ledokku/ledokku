adf97fed06250f3caee549b6b88606c726b19e50
pradel <pradel.leo@gmail.com>
2021-03-29 15:37:53 +0200

    chore(client): fix how config is loaded

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,23 +1,21 @@
 const runConfig: any = window['runConfig' as any];
 
-export const config = runConfig
-  ? {
-      githubClientId: runConfig['GITHUB_CLIENT_ID'],
-      telemetryDisabled: runConfig['TELEMETRY_DISABLED'],
-      serverUrl: '',
-      // If app is loaded over https, we connect with wss
-      serverWsUrl: `${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${
+export const config = {
+  githubClientId: runConfig
+    ? runConfig['GITHUB_CLIENT_ID']
+    : process.env.REACT_APP_GITHUB_CLIENT_ID,
+  telemetryDisabled: runConfig['TELEMETRY_DISABLED'] === '1',
+  serverUrl: process.env.REACT_APP_SERVER_URL
+    ? process.env.REACT_APP_SERVER_URL
+    : '',
+  // If app is loaded over https, we connect with wss
+  serverWsUrl: process.env.REACT_APP_SERVER_URL
+    ? process.env.REACT_APP_SERVER_URL.replace('http', 'ws')
+    : `${window.location.protocol === 'https:' ? 'wss' : 'ws'}://${
         window.location.host
       }`,
-      environment: process.env.NODE_ENV,
-    }
-  : {
-      githubClientId: process.env.REACT_APP_GITHUB_CLIENT_ID,
-      telemetryDisabled: true,
-      serverUrl: process.env.REACT_APP_SERVER_URL,
-      serverWsUrl: process.env.REACT_APP_SERVER_URL?.replace('http', 'ws'),
-      environment: process.env.NODE_ENV,
-    };
+  environment: process.env.NODE_ENV,
+};
 
 export const trackingGoals = {
   createDatabase: 'OHO7WM5Y',
