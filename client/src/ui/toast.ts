7977bd3538fbd9c0eb4845d91cab4e65e96c1da4
pradel <pradel.leo@gmail.com>
2021-04-06 18:03:49 +0200

    chore(client): add options to useToast component

--- client/src/ui/toast.ts
+++ client/src/ui/toast.ts
@@ -1,45 +1,49 @@
-import { useToast as useToastChakra } from '@chakra-ui/react';
+import { useToast as useToastChakra, UseToastOptions } from '@chakra-ui/react';
 
 export const useToast = () => {
   const toastChakra = useToastChakra();
 
-  const error = (description: string) => {
+  const error = (description: string, options?: UseToastOptions) => {
     return toastChakra({
       description: description,
       status: 'error',
       position: 'top-left',
       duration: 5000,
       isClosable: true,
+      ...options,
     });
   };
 
-  const success = (description: string) => {
+  const success = (description: string, options?: UseToastOptions) => {
     return toastChakra({
       description: description,
       status: 'success',
       position: 'top-left',
       duration: 5000,
       isClosable: true,
+      ...options,
     });
   };
 
-  const info = (description: string) => {
+  const info = (description: string, options?: UseToastOptions) => {
     return toastChakra({
       description: description,
       status: 'info',
       position: 'top-left',
       duration: 5000,
       isClosable: true,
+      ...options,
     });
   };
 
-  const warning = (description: string) => {
+  const warning = (description: string, options?: UseToastOptions) => {
     return toastChakra({
       description: description,
       status: 'info',
       position: 'top-left',
       duration: 5000,
       isClosable: true,
+      ...options,
     });
   };
 
