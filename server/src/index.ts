f09cf2e26ca77c7b5269ee187dbb1a0edc998922
arturs <arturs.kirtovskis@gmail.com>
2021-04-25 18:56:44 +0300

    chore(server): remove redundant code

--- server/src/index.ts
+++ server/src/index.ts
@@ -505,9 +505,3 @@ http.listen({ port: 4000 }, () => {
   // When the server boot we start the synchronisation with dokku
   synchroniseServerQueue.add('synchronise-server', {});
 });
-
-const test = async () => {
-  // await prisma.user.deleteMany();
-};
-
-test();
