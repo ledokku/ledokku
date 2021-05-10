581f8023b415d0c3acdde7d826c47c911226110f
arturs <arturs.kirtovskis@gmail.com>
2021-05-10 22:06:32 +0300

    chore(client): fix useeffect dep

--- client/src/pages/app-creation/create-app-github.tsx
+++ client/src/pages/app-creation/create-app-github.tsx
@@ -238,6 +238,7 @@ export const CreateAppGithub = () => {
     reposLoading,
     getBranches,
     selectedRepo?.name,
+    selectedRepo,
   ]);
 
   const handleChangeRepo = (active: RepoOption) => {
