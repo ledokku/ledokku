b0d827a1fa39b6a3f76bc8c189968c89cb0a7297
arturs <arturs.kirtovskis@gmail.com>
2021-05-11 19:15:52 +0300

    chore(client): remove unused import

--- client/src/pages/app/index.tsx
+++ client/src/pages/app/index.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { useHistory, useParams, Link as RouterLink } from 'react-router-dom';
 import { Listbox, Transition } from '@headlessui/react';
 import cx from 'classnames';
