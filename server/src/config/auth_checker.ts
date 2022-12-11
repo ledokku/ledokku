bf99fbc2fed60d81dd3808e0c10bfeb5afd0b038
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2022-12-10 23:42:38 -0600

    Update auth_checker.ts

--- server/src/config/auth_checker.ts
+++ server/src/config/auth_checker.ts
@@ -5,12 +5,6 @@ export const authChecker: AuthChecker<DokkuContext> = async ({
   context,
 }): Promise<boolean> => {
   if (!context.auth) return false;
-  const settings = await context.prisma.settings.findFirst();
-  const user = await context.prisma.user.findUnique({
-    where: { id: context.auth.userId },
-  });
-
-  if (!(settings?.allowedEmails?.includes(user.email) ?? false)) return false;
 
   return true;
 };
