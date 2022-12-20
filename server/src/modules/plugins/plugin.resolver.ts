88382c2989d4901eb9a7f5668c5cd78544d3e629
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2022-12-20 03:22:34 -0600

    Update plugin.resolver.ts

--- server/src/modules/plugins/plugin.resolver.ts
+++ server/src/modules/plugins/plugin.resolver.ts
@@ -4,10 +4,17 @@ import { DokkuPluginRepository } from '../../lib/dokku/dokku.plugin.repository';
 import { DokkuContext } from '../../data/models/dokku_context';
 import { Repository } from '../github/data/models/repository.model';
 import { IsPluginInstalled } from './models/is_plugin_instaled.model';
+import { Plugin } from '../../lib/dokku/models/plugin.model';
 
 @ResolverService(Repository)
 export class PluginResolver {
   constructor(private dokkuPluginRepository: DokkuPluginRepository) {}
+
+  @Authorized()
+  @Query((returns) => [Plugin])
+  async plugins(): Promise<Plugin[]> {
+    return (await this.dokkuPluginRepository.list()).plugins;
+  }
 
   @Authorized()
   @Query((returns) => IsPluginInstalled)
