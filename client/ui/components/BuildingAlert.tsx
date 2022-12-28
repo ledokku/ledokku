ebf2437225dd1380c93b8d6d79006cfbe9125ef8
Ramiro Estrada Garcia <ramiroestradag@gmail.com>
2022-12-28 02:57:44 -0600

    :lipstick: Build alert

--- client/ui/components/BuildingAlert.tsx
+++ client/ui/components/BuildingAlert.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Loading, Text } from "@nextui-org/react";
+import { Button, Card, Text } from "@nextui-org/react";
 import { useRouter } from "next/router";
 import { App } from "../../generated/graphql";
 
@@ -9,24 +9,23 @@ interface BuildingAlertProps {
 export const BuildingAlert = ({ app }: BuildingAlertProps) => {
     const router = useRouter()
 
-    return <Card css={{ backgroundColor: "$primary" }}>
-        <div className='px-4 py-2 flex'>
-            <div>
-                <Loading color="white" />
+    return <Card className="p-1">
+        <div className="animated-gradient"/>
+        <Card variant="flat">
+            <div className='px-4 py-2 flex items-center'>
+                <div className='ml-4 grow'>
+                    <Text h4>{app.name}</Text>
+                    El proyecto {app.name} se está compilando, ya puedes ver los registros en tiempo real.
+                </div>
+                <div>
+                    <Button
+                        ghost
+                        onClick={() => router.push(`/app_build/${app.id}`)}
+                        size="sm">
+                        Ver registros
+                    </Button>
+                </div>
             </div>
-            <div className='ml-4 grow text-white'>
-                <Text h4 className='text-white'>Proyecto {app.name} construyendose</Text>
-                El proyecto {app.name} se está compilando, ya puedes ver los registros en tiempo real.
-            </div>
-            <div className='self-end'>
-                <Button
-                    ghost
-                    onClick={() => router.push(`/app_build/${app.id}`)}
-                    className='text-white border-white'
-                    size="sm">
-                    Ver registros
-                </Button>
-            </div>
-        </div>
+        </Card>
     </Card>;
 }
